7ebf060d3d232b9a988392dc90e8d67d0532f400
vvthanatos <vvthanatos@gmail.com>
2020-04-28 18:19:59 +0300

    [*] code decoration

--- GameEngine/Account.php
+++ GameEngine/Account.php
@@ -176,49 +176,52 @@ class Account {
 		}
 	}
 
-	private function Login() {
-		global $database,$session,$form;
-		$_POST['user'] = mysql_real_escape_string($_POST['user']);
-		if(!isset($_POST['user']) || $_POST['user'] == "") {
-			$form->addError("user",LOGIN_USR_EMPTY);
-		}
-		else if(!$database->checkExist($_POST['user'],0)) {
-			$form->addError("user",USR_NT_FOUND);
-		}
-		if(!isset($_POST['pw']) || $_POST['pw'] == "") {
-			$form->addError("pw",LOGIN_PASS_EMPTY);
-		}
-		else if(!$database->login($_POST['user'],$_POST['pw']) && !$database->sitterLogin($_POST['user'],$_POST['pw'])) {
-			$form->addError("pw",LOGIN_PW_ERROR);
-		}
-		if($database->getUserField($_POST['user'],"act",1) != "") {
-			$form->addError("activate",$_POST['user']);
-		}
-		// Vacation mode by Shadow
-		if($database->getUserField($_POST['user'],"vac_mode",1) == 1 && $database->getUserField($_POST['user'],"vac_time",1) > time()) {
-		$form->addError("vacation","Vacation mode is still enabled");
-		}
-		// Vacation mode by Shadow
-		if($form->returnErrors() > 0) {
-			$_SESSION['errorarray'] = $form->getErrors();
-			$_SESSION['valuearray'] = $_POST;
-
-			header("Location: login.php");
-		}
-		else {
-		$userid = $database->getUserArray($_POST['user'], 0);
-		// Vacation mode by Shadow
-		$database->removevacationmode($userid['id']);
-		// Vacation mode by Shadow
-		if($database->login($_POST['user'],$_POST['pw'])){
-			$database->UpdateOnline("login" ,$_POST['user'],time(),$userid['id']);
-		}else if($database->sitterLogin($_POST['user'],$_POST['pw'])){
-			$database->UpdateOnline("sitter" ,$_POST['user'],time(),$userid['id']);
-		}
-			setcookie("COOKUSR",$_POST['user'],time()+COOKIE_EXPIRE,COOKIE_PATH);
-			$session->login($_POST['user']);
-		}
-	}
+	private function Login()
+    {
+        global $database, $session, $form;
+        
+        $_POST['user'] = $database->realEscapeString($_POST['user']);
+        if (!isset($_POST['user']) || $_POST['user'] == "") {
+            $form->addError("user", LOGIN_USR_EMPTY);
+        }
+        elseif (!$database->checkExist($_POST['user'], 0)) {
+            $form->addError("user", USR_NT_FOUND);
+        }
+        if (!isset($_POST['pw']) || $_POST['pw'] == "") {
+            $form->addError("pw", LOGIN_PASS_EMPTY);
+        }
+        elseif (!$database->login($_POST['user'], $_POST['pw']) && !$database->sitterLogin($_POST['user'], $_POST['pw'])) {
+            $form->addError("pw", LOGIN_PW_ERROR);
+        }
+        if ($database->getUserField($_POST['user'], "act", 1) != "") {
+            $form->addError("activate", $_POST['user']);
+        }
+        // Vacation mode by Shadow
+        if ($database->getUserField($_POST['user'], "vac_mode", 1) == 1 && $database->getUserField($_POST['user'], "vac_time", 1) > time()) {
+            $form->addError("vacation", "Vacation mode is still enabled");
+        }
+        // Vacation mode by Shadow
+        if ($form->returnErrors() > 0) {
+            $_SESSION['errorarray'] = $form->getErrors();
+            $_SESSION['valuearray'] = $_POST;
+            
+            header("Location: login.php");
+        }
+        else {
+            $userid = $database->getUserArray($_POST['user'], 0);
+            // Vacation mode by Shadow
+            $database->removevacationmode($userid['id']);
+            // Vacation mode by Shadow
+            if ($database->login($_POST['user'], $_POST['pw'])) {
+                $database->UpdateOnline("login", $_POST['user'], time(), $userid['id']);
+            }
+            elseif ($database->sitterLogin($_POST['user'], $_POST['pw'])) {
+                $database->UpdateOnline("sitter", $_POST['user'], time(), $userid['id']);
+            }
+            setcookie("COOKUSR", $_POST['user'], time() + COOKIE_EXPIRE, COOKIE_PATH);
+            $session->login($_POST['user']);
+        }
+    }
 
 	private function Logout() {
 		global $session,$database;
@@ -256,6 +259,6 @@ class Account {
 		$message->sendWelcome($uid,$username);
 	}
 
-};
+}
 
 $account = new Account();
--- GameEngine/Building.php
+++ GameEngine/Building.php
@@ -60,43 +60,48 @@ class Building {
         	}
     	}  
 
-    public function procBuild($get) {
+    public function procBuild($get)
+    {
         global $session, $village, $database;
-        if(isset($get['a']) && $get['c'] == $session->checker && !isset($get['id'])) {
-            if($get['a'] == 0) {
+        
+        if (isset($get['a']) && $get['c'] == $session->checker && !isset($get['id'])) {
+            if ($get['a'] == 0) {
                 $this->removeBuilding($get['d']);
-            }else {
+            } else {
                 $session->changeChecker();
-                $this->canProcess($village->resarray['f'.$get['a'].'t'],$get['a']);
+                $this->canProcess($village->resarray[ 'f' . $get['a'] . 't' ], $get['a']);
                 $this->upgradeBuilding($get['a']);
             }
         }
-        if(isset($get['master']) && isset($get['id']) && isset($get['time']) && $session->gold >= 1 && $session->goldclub && $village->master == 0 && (isset($get['c']) && $get['c']== $session->checker)) {
-            $m=$get['master'];
+        
+        if (isset($get['master']) && isset($get['id']) && isset($get['time']) && $session->gold >= 1 && $session->goldclub && $village->master == 0 && (isset($get['c']) && $get['c'] == $session->checker)) {
+            $m = $get['master'];
             $master = $_GET;
             $session->changeChecker();
-            if($session->access==BANNED){
+            if ($session->access == BANNED) {
                 header("Location: banned.php");
                 exit;
-            }    
+            }
             $level = $database->getResourceLevel($village->wid);
-            $database->addBuilding($village->wid, $get['id'], $get['master'], 1, $get['time'], 1, $level['f'.$get['id']] + 1 + count($database->getBuildingByField($village->wid,$get['id'])));
-            $database->modifyGold($session->uid,1,0);
-            if($get['id'] > 18) {
+            $database->addBuilding($village->wid, $get['id'], $get['master'], 1, $get['time'], 1, $level[ 'f' . $get['id'] ] + 1 + count($database->getBuildingByField($village->wid, $get['id'])));
+            $database->modifyGold($session->uid, 1, 0);
+            if ($get['id'] > 18) {
                 header("Location: dorf2.php");
             } else {
                 header("Location: dorf1.php");
             }
         }
-        if(isset($get['a']) && $get['c'] == $session->checker && isset($get['id'])) {
-            if  ($get['id'] > 18 && ($get['id'] < 41 || $get['id'] == 99)){
-            	$session->changeChecker();
-            	$this->canProcess($get['a'],$get['id']);
-            	$this->constructBuilding($get['id'],$get['a']);
+        
+        if (isset($get['a']) && $get['c'] == $session->checker && isset($get['id'])) {
+            if ($get['id'] > 18 && ($get['id'] < 41 || $get['id'] == 99)) {
+                $session->changeChecker();
+                $this->canProcess($get['a'], $get['id']);
+                $this->constructBuilding($get['id'], $get['a']);
             }
         }
-        if(isset($get['buildingFinish']) && $session->plus) {
-            if($session->gold >= 2 && $session->sit == 0) {
+        
+        if (isset($get['buildingFinish']) && $session->plus) {
+            if ($session->gold >= 2 && $session->sit == 0) {
                 $this->finishAll();
             }
         }
