2712cf5189b401a75c51338c725adb0f721228b3
vvthanatos <vvthanatos@gmail.com>
2020-05-29 11:59:19 +0300

    [*] karte fix 3

--- App/Controllers/Map/AbstractMapController.php
+++ App/Controllers/Map/AbstractMapController.php
@@ -3,8 +3,25 @@
 namespace App\Controllers\Map;
 
 use App\Controllers\AbstractController;
+use HalimonAlexander\Registry\Registry;
 
 class AbstractMapController extends AbstractController
 {
     protected $bundle = 'map';
+    
+    /** @var \GameEngine\Database\MysqliModel */
+    protected $database;
+    
+    /** @var \GameEngine\Session */
+    protected $session;
+    
+    public function __construct()
+    {
+        parent::__construct();
+    
+        $registry = Registry::getInstance();
+    
+        $this->database = $registry->get('database');
+        $this->session = $registry->get('session');
+    }
 }
